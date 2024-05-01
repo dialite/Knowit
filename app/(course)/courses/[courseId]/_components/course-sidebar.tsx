285abf1b5dad94b3c7b7152dd94d9fd254f494fd
dialite <raymondolisa01@gmail.com>
2024-05-01 22:06:31 +0100

    Initialize course progress indicator and changed mysql storage to local

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -3,6 +3,7 @@ import { Chapter, Course, UserProgress } from "@prisma/client"
 import { redirect } from "next/navigation"
 
 import { db } from "@/lib/db"
+import { CourseProgress } from "@/components/course-progress"
 
 import { CourseSidebarItem } from "./course-sidebar-item"
 
@@ -37,7 +38,11 @@ export const CourseSidebar = async({course, progressCount}: CourseSidebarProps) 
                 <h1 className="font-semibold">
                     {course.title}
                 </h1>
-                {/* Check purchase and add progress */}
+                {purchase && (
+                    <div className="mt-10">
+                        <CourseProgress variant="success" value={progressCount}/>
+                    </div>
+                )}
             </div>
             <div className="flex flex-col w-full">
                 {course.chapters.map((chapter) => (
