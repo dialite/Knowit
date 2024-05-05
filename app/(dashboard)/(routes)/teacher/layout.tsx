003fac55a0c988396ce46da0a634125a140a9289
dialite <raymondolisa01@gmail.com>
2024-05-05 21:08:22 +0100

    Add redirect features for accounts not permitted to use teacher mode

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -3,11 +3,11 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
-    const {userId} = auth()
+  const { userId } = auth();
 
-    if (!isTeacher(userId)) {
-        return redirect("/")
-    }
+  if (!isTeacher(userId)) {
+    return redirect("/");
+  }
   return <>{children}</>;
 };
 
